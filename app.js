b4d18b7850966dfb241dec2f15b9525b7568ef71
Sergey Popov <sergeykms@gmail.com>
2021-05-20 21:44:29 +1000

    20_05_2021___1

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/users');
 const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use('/', routes);
+app.use('/users', require('./routes/users'));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+  next();
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
--- controllers/users.js
+++ controllers/users.js
@@ -7,9 +7,8 @@ module.exports.getAllUsers = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: err.message }));
+  const {name, about, avatar} = req.body;
+  User.create({name, about, avatar})
+    .then(user => res.send({data: user}))
+    .catch(err => res.status(500).send({message: err.message}));
 };
